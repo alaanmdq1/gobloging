5f112990ab929aa9e5f203af2586ba9b7341f02f
Alan <alaan.mdq@live.com>
2021-01-13 15:30:32 -0300

    Base de datos conectada

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"log"
+
+	"github.com/alaanmdq1/gobloging/bd"
+	"github.com/alaanmdq1/gobloging/handlers"
+)
+
 func main() {
-	
+	if bd.ChequeoConnection() == 0 {
+		log.Fatal("Sin conexión a la BD")
+		return
+	}
+	handlers.Manejadores()
+
 }
