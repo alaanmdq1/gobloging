e906087321d4c5174a22c588429f02c8cb96737e
Alan <alaan.mdq@live.com>
2021-02-03 09:51:26 -0300

    ultimo commit

---
+++ routers/altaRelacion.go
@@ -0,0 +1,36 @@
+package routers
+
+import (
+	"net/http"
+
+	"github.com/alaanmdq1/gobloging/bd"
+	"github.com/alaanmdq1/gobloging/models"
+)
+
+/*AltaRelacion - realiza el registro de la relación entre usuarios*/
+func AltaRelacion(w http.ResponseWriter, r *http.Request) {
+	// Obtengo el ID que viene como Parámetro
+	ID := r.URL.Query().Get("id")
+	if len(ID) < 1 {
+		http.Error(w, "El parámetro ID es obligatorio", http.StatusBadRequest)
+		return
+	}
+	//Definimos un modelo relación en donde guardaremos lo que vamos a grabar en la bd
+	var rel models.Relacion
+	//Colocamos como UsuarioID al que tenemos grabado en la variable glogal, que es el logueado
+	rel.UsuarioID = IDUsuario
+	//Colocamos como UsuarioRelacionID al que viene como parámetro en el query
+	rel.UsuarioRelacionID = ID
+
+	status, err := bd.InsertoRelacion(rel)
+	//si hay un error
+	if err != nil {
+		http.Error(w, "Ocurrió un error al intentar insertar relación, intentelo nuevamente. "+err.Error(), http.StatusBadRequest)
+		return
+	}
+	if status == false {
+		http.Error(w, "No se ha logrado insertar la relación. "+err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
